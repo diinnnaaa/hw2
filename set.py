95c06a0dd37eb727e9f8869c1ffb329dfb17376a
diinnnaaa <149644597+diinnnaaa@users.noreply.github.com>
2023-11-15 10:45:44 +0600

    SETтан работа

---
+++ set.py
@@ -0,0 +1,51 @@
+#дубликаттар жок
+#Реты жок
+#{}
+#коп елементтты коссак update if one word use add
+#remove элкментты алады
+# pop рандомайзер и убирает
+# cities = {'Astana', 'Almaty','Karaganda','Petropavl','Kokshetau'}
+# print(len(cities))
+# new_cit = input("Enter a city: ")
+# cities.add(new_cit)
+# print (cities)
+
+# cities.add('Mangystau')
+# print(len(cities))
+
+
+# new_cit = input("Enter a city: ")
+# if new_cit in cities:
+#     cities.remove(new_cit)
+#     print(f"{new_cit} was removed from the set ")
+# else:
+#     print("No such city")
+
+
+# cities= {"almaty", "Astana", "Aktau"}
+# cit = input("City")
+# p = cit.split()
+# cities.update(p)
+# print(cities)
+
+#--------------1
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+print(len(it_companies))
+
+#--------------2pflfybt
+it_companies.add('twitter')
+print(it_companies)
+
+#----------------3egh
+it_companies.update(['vk', 'agent', 'mailru'])
+print(it_companies)
+
+#----------------4egh
+it_companies.remove('Google')
+print(it_companies)
+
+#----------------5egh
+# тем что один полночтью всё удаляет а другой жай очищает 
+
+#------------------lvl 2
+A = {}
